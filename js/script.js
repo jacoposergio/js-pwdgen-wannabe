fb8746334f137c2eccab8a66dfb67ddbb40e931e
jacoposergio <djago32@gmail.com>
2022-05-31 16:39:07 +0200

    nome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-document.getElementById('mio-titolo').innerHTML = 'Hello world!';
+// Chiediamo all'utente il suo nome,
+//  il cognome, colore prefeito e aggiungiamo 21
+
+// 1.chiediamo il nome 
+
+
+const userName = prompt('Dimmi il tuo nome');
