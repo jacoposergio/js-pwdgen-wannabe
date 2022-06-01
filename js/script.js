50762378040dec73c290e2a25711332e6a195d6f
jacoposergio <djago32@gmail.com>
2022-06-01 09:45:34 +0200

    consolelog

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 // Chiediamo all'utente il suo nome,
-//  il cognome, colore prefeito e aggiungiamo 21
+//  il cognome, colore preferito e aggiungiamo 21
 
 // 1. chiediamo il nome 
 
 const userName = prompt('Qual è il tuo nome?');
+console.log(userName)
 
 
 // 2. chiediamo il cognome 
 
 const userSurname = prompt('Qual è il tuo cognome?')
+console.log(userSurname)
 
 
 // 3. chiediamo il colore preferito  
 
 const userColor = prompt('Qual è il tuo colore preferito?')
-
+console.log(userColor)
 
 // 4. costruiamo il messaggio finale per l'utente
+
 const userMessage = 'Il tuo NickName è' + userName + userSurname + userColor + '21';
+console.log(userMessage)
 
 
 // stampiamo il messaggio
+
 document.getElementById('result').innerHTML = userMessage;
 
 
